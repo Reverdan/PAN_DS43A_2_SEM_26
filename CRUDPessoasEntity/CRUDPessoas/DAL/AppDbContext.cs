888c121131a1f37a7001db1224586ca1a4b9d6a9
Reverdan Almeida Sparinger <reverdan@gmail.com>
2026-09-15 17:29:50 -0300

    Entity

---
+++ CRUDPessoasEntity/CRUDPessoas/DAL/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CRUDPessoas.modelo;
+using Microsoft.EntityFrameworkCore;
+
+
+//Microsoft.EntityFrameworkCore
+//Microsoft.EntityFrameworkCore.SqlServer
+//Microsoft.EntityFrameworkCore.Tools
+
+/*
+ * No Visual Studio, vá no menu superior em Ferramentas (Tools) > Gerenciador de Pacotes NuGet > Console do Gerenciador de Pacotes.
+ * 
+ *    Add-Migration Inicial
+ *    Update-Database
+ *    
+ */
+
+
+namespace CRUDPessoas.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=REVER-NOTE\SQLEXPRESS;
+            Initial Catalog=ds34a;User ID=sa;
+            Password=rever;Encrypt=False");
+        }
+    }
+}
---
+++ CRUDPessoasEntity/CRUDPessoas/Migrations/20260915194045_inicial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDPessoas.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    rg = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    cpf = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
